d1c82db117a6fa05e9fbf1e65ea65e3496e20447
heinrichschiller <heinrich@heinrich-schiller.de>
2024-01-29 11:56:04 +0100

    rewrite, check if vs code is not installed, ubuntu only

--- install.sh
+++ install.sh
@@ -1,59 +1,35 @@
 #!/usr/bin/env bash
 
-# Install python-nautilus
-echo "Installing python-nautilus..."
-if type "pacman" > /dev/null 2>&1
+if ! command -v code > /dev/null 2>&1
 then
-    # check if already install, else install
-    pacman -Qi python-nautilus &> /dev/null
-    if [ `echo $?` -eq 1 ]
-    then
-        sudo pacman -S --noconfirm python-nautilus
-    else
-        echo "python-nautilus is already installed"
-    fi
-elif type "apt-get" > /dev/null 2>&1
-then
-    # Find Ubuntu python-nautilus package
-    package_name="python3-nautilus"
-    found_package=$(apt-cache search --names-only $package_name)
-    if [ -z "$found_package" ]
-    then
-        package_name="python-nautilus"
-    fi
-
-    # Check if the package needs to be installed and install it
-    installed=$(apt list --installed $package_name -qq 2> /dev/null)
-    if [ -z "$installed" ]
-    then
-        sudo apt-get install -y $package_name
-    else
-        echo "$package_name is already installed."
-    fi
-elif type "dnf" > /dev/null 2>&1
-then
-    installed=`dnf list --installed nautilus-python 2> /dev/null`
-    if [ -z "$installed" ]
-    then
-        sudo dnf install -y nautilus-python
-    else
-        echo "nautilus-python is already installed."
-    fi
-else
-    echo "Failed to find python-nautilus, please install it manually."
+    echo "VS Code not found. Please install VS Code first"
+    exit 1
 fi
 
-# Remove previous version and setup folder
+package_name="python3-nautilus"
+found_package=$(apt-cache search --names-only $package_name)
+if [ -z "$found_package" ]
+then
+    package_name="python-nautilus"
+fi
+
+installed=$(apt list --installed $package_name -qq 2> /dev/null)
+if [ -z "$installed" ]
+then
+    sudo apt-get install -y $package_name
+else
+    echo "$package_name is already installed."
+fi
+
 echo "Removing previous version (if found)..."
+
 mkdir -p ~/.local/share/nautilus-python/extensions
-rm -f ~/.local/share/nautilus-python/extensions/VSCodeExtension.py
+
 rm -f ~/.local/share/nautilus-python/extensions/code-nautilus.py
 
-# Download and install the extension
 echo "Downloading newest version..."
-wget --show-progress -q -O ~/.local/share/nautilus-python/extensions/code-nautilus.py https://raw.githubusercontent.com/harry-cpp/code-nautilus/master/code-nautilus.py
+wget --show-progress -q -O ~/.local/share/nautilus-python/extensions/code-nautilus.py https://raw.githubusercontent.com/heinrichschiller/vs-code-nautilus-extension/main/code-nautilus.py
 
-# Restart nautilus
 echo "Restarting nautilus..."
 nautilus -q
 
